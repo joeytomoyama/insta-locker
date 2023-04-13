0831abb3bc83a46c56dc70800fd76bd8acde1064
Joey Prüssing <j.pruessing28@gmail.com>
2023-04-13 02:49:09 +0200

    first draft. program runs indefinitely. specific key press activates program. pixel color values of specific x,y point can be prompted.

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
+import threading
+import time
+import pyautogui
 import win32gui
 import win32api
+import keyboard
 
-# Define the coordinates of the pixel
+print('start')
+
+# Define the coordinates of the bottom left pixel
 x = 0
-y = 1079
+y = 0
+
+active = False
+
+# Define the color of the pixel to trigger the key press
+white = (255, 255, 255)
 
 # Get the handle to the desktop window
 hDesktop = win32gui.GetDesktopWindow()
 
-# Get the device context for the entire screen
-hDC = win32gui.GetWindowDC(hDesktop)
+# Define the function to run continuously in the background
+def run():
+    global active
+    while True:
+        if active == False:
+            print('nothing happening')
+            time.sleep(0.5)
+            continue
 
-# Get the pixel value at the specified location
-pixel = win32gui.GetPixel(hDC, x, y)
+        mouseLocation = pyautogui.position()
 
-# Release the device context
-win32gui.ReleaseDC(hDesktop, hDC)
+        # Get the device context for the entire screen
+        hDC = win32gui.GetWindowDC(hDesktop)
 
-# Print the pixel value as RGB tuple
-print("Pixel value:", (pixel & 0xff), ((pixel >> 8) & 0xff), ((pixel >> 16) & 0xff))
+        # Get the pixel value at the specified location
+        # pixel = win32gui.GetPixel(hDC, x, y)
+        pixel = win32gui.GetPixel(hDC, mouseLocation.x, mouseLocation.y)
+
+        # Release the device context
+        win32gui.ReleaseDC(hDesktop, hDC)
+
+        # Print the pixel value as RGB tuple
+        print("Pixel value:", (pixel & 0xff), ((pixel >> 8) & 0xff), ((pixel >> 16) & 0xff))
+        # print(pyautogui.position())a
+
+        # Wait for a short time before checking again
+        time.sleep(0.1)
+
+def listenForKey(key):
+    global active
+    if key.name == 'a':
+        active = not active
+
+keyboard.on_press(listenForKey)
+
+# Create a new thread and start running the function in the background
+thread = threading.Thread(target=run)
+thread.daemon = True
+thread.start()
+
+# Program will continue to run in the background indefinitely
+while True:
+    time.sleep(0.1)
